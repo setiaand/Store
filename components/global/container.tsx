30bb8ff55957513ad467a32b658cf399f49cac5b
Prass <cahnengrat921@gmail.com>
2025-04-07 21:56:02 +0700

    provide component container

---
+++ components/global/container.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+
+export default function Container({
+    children,
+    className,
+}: {
+    children:React.React.ReactNode;
+    className?:string;
+}) {
+    return <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+        {children}
+    </div>
+}
