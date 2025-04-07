57be3317bbae2a38708b34cae6a92e5133df387c
Prass <cahnengrat921@gmail.com>
2025-04-07 21:19:54 +0700

    provide button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return <h1 className='text-3xl'>HomePage</h1>;
+  return <div>
+    <h1 className='text-3xl'>HomePage</h1>
+    <Button variant={"secondary"} size={'lg'} className="capitalize m-8">Cllick Me</Button>
+  </div>
 }
